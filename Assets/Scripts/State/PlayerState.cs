888182fc6487ab0f662b80fbd9605da9eefbbf11
sergey.binder <sergey.binder@matryoshka.com>
2020-05-30 15:18:38 +0700

    Added primitive PlayerState with inventory

---
+++ Assets/Scripts/State/PlayerState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace STP.State {
+    public class PlayerState {
+        public const int MaxFuel = 50;
+        
+        static PlayerState _instance;
+        public static PlayerState Instance {
+            get {
+                if ( _instance == null ) {
+                    _instance = new PlayerState();
+                }
+                return _instance;
+            }
+        }
+
+        public readonly Dictionary<string, int> Inventory = new Dictionary<string, int>();
+
+        int _fuel = 0;
+
+        public int Fuel {
+            get => _fuel;
+            set {
+                if ( (value < 0) || (value > MaxFuel) ) {
+                    Debug.LogWarningFormat("New fuel value is outside the acceptable range: '{0}'", value);
+                    value = Mathf.Clamp(value, 0, MaxFuel);
+                }
+                _fuel = value;
+            }
+        }
+    }
+}
