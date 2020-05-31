a4ec359b315cf0b27e075d59af0bf862f90c728f
alexandr.shelekhin <ultrashel@gmail.com>
2020-05-31 14:56:37 +0700

    Added stub images for gameplay elements

---
+++ Assets/Scripts/Stubs/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public string SceneToLoad;
+
+    public void LoadScene() {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
