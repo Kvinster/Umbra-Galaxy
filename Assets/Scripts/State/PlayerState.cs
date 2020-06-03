b8246fc28053258ca19be254e522df17c4603d55
sergey.binder <sergey.binder@matryoshka.com>
2020-06-03 19:15:08 +0700

    Added events for fuel and inventory change in PlayerState

--- Assets/Scripts/State/PlayerState.cs
+++ Assets/Scripts/State/PlayerState.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+using System;
 using System.Collections.Generic;
 
 namespace STP.State {
@@ -17,19 +18,50 @@ namespace STP.State {
             }
         }
 
-        public readonly Dictionary<string, int> Inventory = new Dictionary<string, int>();
+        readonly Dictionary<string, int> _inventory = new Dictionary<string, int>();
 
         int _fuel = 0;
-
         public int Fuel {
             get => _fuel;
             set {
+                if ( _fuel == value ) {
+                    return;
+                }
                 if ( (value < 0) || (value > MaxFuel) ) {
                     Debug.LogWarningFormat("New fuel value is outside the acceptable range: '{0}'", value);
                     value = Mathf.Clamp(value, 0, MaxFuel);
                 }
                 _fuel = value;
+                OnFuelChanged?.Invoke(_fuel);
             }
+        }
+
+        public event Action OnInventoryChanged;
+        public event Action<int> OnFuelChanged;
+
+        public bool HasInInventory(string itemName, int amount = 1) {
+            return _inventory.ContainsKey(itemName) && (_inventory[itemName] >= amount);
+        }
+
+        public bool TryTakeFromInventory(string itemName, int amount = 1) {
+            if ( HasInInventory(itemName, amount) ) {
+                _inventory[itemName] -= amount;
+                if ( _inventory[itemName] <= 0 ) {
+                    _inventory.Remove(itemName);
+                }
+                OnInventoryChanged?.Invoke();
+                return true;
+            }
+            return false;
+        }
+
+        public void AddToInventory(string itemName, int amount) {
+            if ( _inventory.ContainsKey(itemName) ) {
+                _inventory[itemName] += amount;
+            } else {
+                _inventory.Add(itemName, amount);
+            }
+            OnInventoryChanged?.Invoke();
         }
     }
 }
