fd3015da34c42ff422b27f4bb2cdf94b029f434b
sergey.binder <sergey.binder@matryoshka.com>
2020-07-31 19:46:07 +0700

    Different sprites for AI ships

---
+++ Assets/Scripts/Behaviour/Utils/RandomSprite.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+
+namespace STP.Behaviour.Utils {
+    public sealed class RandomSprite : MonoBehaviour {
+        public SpriteRenderer SpriteRenderer;
+        [Space]
+        public List<Sprite> Sprites = new List<Sprite>();
+
+        void Reset() {
+            SpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        void Start() {
+            SpriteRenderer.sprite = ((Sprites == null) || (Sprites.Count == 0))
+                ? null
+                : Sprites[Random.Range(0, Sprites.Count)];
+        }
+    }
+}
