c9c2ca4a463cdbcc373264214febbbebb12cfff6
sergey.binder <sergey.binder@matryoshka.com>
2020-08-01 13:45:53 +0700

    Tooltips for player UI

---
+++ Assets/Scripts/Behaviour/Utils/HoverTooltip.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace STP.Behaviour.Utils {
+    public sealed class HoverTooltip : MonoBehaviour {
+        public EventTrigger EventTrigger;
+        public GameObject   Root;
+
+        bool _showInt;
+        bool _showExt;
+
+        bool ShowInt {
+            get => _showInt;
+            set {
+                _showInt = value;
+                SetShown(_showInt || _showExt);
+            }
+        }
+
+        bool ShowExt {
+            get => _showExt;
+            set {
+                _showExt = value;
+                SetShown(_showInt || _showExt);
+            }
+        }
+
+        void Start() {
+            var entry = new EventTrigger.Entry { eventID = EventTriggerType.PointerEnter };
+            entry.callback.AddListener(_ => ShowInt = true);
+            EventTrigger.triggers.Add(entry);
+
+            entry = new EventTrigger.Entry { eventID = EventTriggerType.PointerExit };
+            entry.callback.AddListener(_ => ShowInt = false);
+            EventTrigger.triggers.Add(entry);
+
+            Root.SetActive(false);
+        }
+
+        public void OnPointerEnterExt() {
+            ShowExt = true;
+        }
+
+        public void OnPointerExitExt() {
+            ShowExt = false;
+        }
+
+        void SetShown(bool isShown) {
+            Root.SetActive(isShown);
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/Utils/TooltipButtonInspector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+using STP.Behaviour.Utils;
+
+namespace STP.Editor.Utils {
+    [CustomEditor(typeof(TooltipButton))]
+    public sealed class TooltipButtonInspector : ButtonEditor {
+        public override void OnInspectorGUI() {
+            EditorGUILayout.LabelField("Default Button", EditorStyles.boldLabel);
+            base.OnInspectorGUI();
+            
+            EditorGUILayout.LabelField("Tooltip Button", EditorStyles.boldLabel);
+            var tooltipButton = target as TooltipButton;
+            if ( !tooltipButton ) {
+                return;
+            }
+            var tooltipProp = serializedObject.FindProperty("Tooltip");
+            if ( tooltipProp != null ) {
+                EditorGUILayout.PropertyField(tooltipProp);
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
