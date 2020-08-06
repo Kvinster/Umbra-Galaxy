283ed83bcdd2e63d9df7e958f19ef24b0965bae0
alexandr.shelekhin <alexandr.shelekhin@matryoshka.com>
2020-08-06 21:07:25 +0700

    added singleton

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,16 @@
+﻿namespace STP.Utils {
+    public abstract class Singleton<T> where T : Singleton<T>, new() {
+        static T _instance;
+        public static T Instance {
+            get {
+                if ( _instance == null ) {
+                    _instance = new T();
+                    _instance.Init();
+                }
+                return _instance;
+            }
+        }
+        
+        protected abstract void Init();
+    }
+}
