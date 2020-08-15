14596a7f61f905d7bd3633b788d3afbbf0263d71
alexandr.shelekhin <alexandr.shelekhin@matryoshka.com>
2020-08-15 21:45:48 +0700

    Added room to the core level

--- Assets/Scripts/Behaviour/Core/Objects/DoorObject/Door.cs
+++ Assets/Scripts/Behaviour/Core/Objects/DoorObject/Door.cs
@@ -2,49 +2,77 @@
 
 using STP.Gameplay;
 using STP.Utils;
-using System;
+using STP.Utils.GameComponentAttributes;
 
 namespace STP.Behaviour.Core.Objects.DoorObject {
     public class Door : CoreComponent {
         public float OpeningTime = 3f;
         
-        DoorState _state = DoorState.Closed;
+        [NotNull] public DoorFrame LeftFrame;
+        [NotNull] public DoorFrame RightFrame;
         
-        Timer _timer = new Timer();
+        readonly Timer _timer = new Timer();
+        
+        DoorState _state = DoorState.Closed;
+
+        DoorState State {
+            get => _state;
+            set {
+                LeftFrame.SetDoorState(value);
+                LeftFrame.SetProgress(_timer.NormalizedProgress);
+                
+                RightFrame.SetDoorState(value);
+                RightFrame.SetProgress(_timer.NormalizedProgress);
+                
+                _state = value;
+            }
+        }
+
         public override void Init(CoreStarter starter) {
+            LeftFrame.Init();
+            RightFrame.Init();
         } 
 
         void OnTriggerEnter2D(Collider2D other) {
-            _state = DoorState.Opening;
-             var passedTime = ( _state == DoorState.Closing ) ? (OpeningTime - _timer.TimePassed) : 0f;
+            if ( !other.gameObject.GetComponent<PlayerShip>() ) {
+                return;
+            }
+            var passedTime = ( State == DoorState.Closing ) ? _timer.LeftTime : 0f;
+            State = DoorState.Opening;
             _timer.Start(OpeningTime, passedTime);
         }
 
         void OnTriggerExit2D(Collider2D other) {
-            _state = DoorState.Closing;
-            var passedTime = ( _state == DoorState.Opening ) ? (OpeningTime - _timer.TimePassed) : 0f;
+            if ( !other.gameObject.GetComponent<PlayerShip>() ) {
+                return;
+            }
+            var passedTime = ( State == DoorState.Opening ) ? _timer.LeftTime : 0f;
+            State = DoorState.Closing;
             _timer.Start(OpeningTime, passedTime);
         }
 
         void Update() {
-            if ( !_timer.DeltaTick() ) {
-                return;
-            }
-            switch ( _state ) {
-                case DoorState.Opening:
-                    _state = DoorState.Opened;
-                    _timer.Stop();
-                    break;
-                case DoorState.Closing:
-                    _state = DoorState.Closed;
-                    _timer.Stop();
-                    break;
-                case DoorState.Closed:
-                case DoorState.Opened:
+            if ( _timer.DeltaTick() ) {
+                switch ( State ) {
+                    case DoorState.Opening:
+                        State = DoorState.Opened;
+                        _timer.Stop();
                         break;
-                default:
-                    Debug.LogError($"Unknown door state {_state}", this);
-                    break;
+                    case DoorState.Closing:
+                        State = DoorState.Closed;
+                        _timer.Stop();
+                        break;
+                    case DoorState.Closed:
+                    case DoorState.Opened:
+                        break;
+                    default:
+                        Debug.LogError($"Unknown door state {State}", this);
+                        break;
+                }
+            }
+            else {
+                LeftFrame.SetProgress(_timer.NormalizedProgress);
+                RightFrame.SetProgress(_timer.NormalizedProgress);
             }
         }
     }
---
+++ Assets/Scripts/Behaviour/Core/Objects/DoorObject/DoorFrame.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+using STP.Utils;
+
+namespace STP.Behaviour.Core.Objects.DoorObject {
+    public sealed class DoorFrame : GameBehaviour {
+        Vector2 _openedPosition;
+        Vector2 _closedPosition;
+
+        Vector2 _from;
+        Vector2 _to;
+        
+        public void Init() {
+            var rectTransform = transform as RectTransform;
+            _closedPosition = rectTransform.localPosition;
+            _openedPosition = _closedPosition - (Vector2)(rectTransform.localRotation * new Vector2(0f, rectTransform.sizeDelta.y));
+            SetDoorState(DoorState.Closed);
+            SetProgress(1);
+        }
+
+        public void SetDoorState(DoorState state) {
+            switch ( state ) {
+                case DoorState.Closed:
+                    _to   = _closedPosition;
+                    _from = _closedPosition;
+                    break;
+                case DoorState.Opened:
+                    _to   = _openedPosition;
+                    _from = _openedPosition;
+                    break;
+                case DoorState.Opening:
+                    _from = _closedPosition;
+                    _to   = _openedPosition;
+                    break;
+                case DoorState.Closing:
+                    _from = _openedPosition;
+                    _to   = _closedPosition;
+                    break;
+                default:
+                    Debug.LogError("Unknown door state");
+                    break;
+            }
+        }
+
+        public void SetProgress(float progress) {
+            transform.localPosition = Vector2.Lerp(_from, _to, progress);
+        }
+    }
+}
