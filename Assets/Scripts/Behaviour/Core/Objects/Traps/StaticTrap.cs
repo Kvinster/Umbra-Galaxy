c4ff1809b934acd43bc4640e2079a6aadf984bf4
alexandr.shelekhin <ultrashel@gmail.com>
2020-08-22 13:42:06 +0700

    added static traps

---
+++ Assets/Scripts/Behaviour/Core/Objects/Traps/StaticTrap.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+using STP.Gameplay;
+using STP.Utils;
+
+namespace STP.Behaviour.Core.Objects.Traps {
+    public class StaticTrap : GameBehaviour {
+        const float Damage = 5;
+        void OnCollisionEnter2D(Collision2D other) {
+            var destructableComp = other.gameObject.GetComponent<IDestructable>();
+            destructableComp?.GetDamage(Damage);
+        }
+    }
+}
