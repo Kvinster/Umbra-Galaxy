58b6b031dcbf5cd0ea847dd8fdff194ed822face
alexandr.shelekhin <ultrashel@gmail.com>
2020-08-27 22:43:11 +0700

    fixed core level borders

--- Assets/Scripts/Behaviour/Core/Objects/Border.cs
+++ Assets/Scripts/Behaviour/Core/Objects/Border.cs
@@ -1,5 +1,35 @@
-﻿using STP.Utils;
+﻿using UnityEditor;
+using UnityEngine;
+
+using STP.Behaviour.Starter;
+using STP.Gameplay;
+using STP.State.Core;
 
 namespace STP.Behaviour.Core.Objects {
-    public class Border : GameComponent { }
+    public class Border : CoreComponent {
+        public int BorderRadius = 2000;
+        
+        CoreShipState      _playerState;
+        SelfDestructEngine _selfDestructEngine;
+        
+        public override void Init(CoreStarter starter) {
+            _playerState        = starter.CoreManager.CorePlayerShipState;
+            _selfDestructEngine = starter.CoreManager.SelfDestructEngine;
+        }
+
+        void Update() {
+            var distance = (_playerState.Position - (Vector2)transform.position).magnitude;
+            
+            if ( distance > BorderRadius && !_selfDestructEngine.IsActive) {
+                _selfDestructEngine.StartSelfDestruction();
+            }
+            if ( distance <= BorderRadius && _selfDestructEngine.IsActive) {
+                _selfDestructEngine.StopSelfDestruction();
+            }
+        }
+
+        void OnDrawGizmos() {
+            Handles.DrawWireDisc(transform.position, Vector3.forward, BorderRadius);
+        }
+    }
 }
