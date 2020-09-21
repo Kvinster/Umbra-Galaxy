9fabce7c39ff6b93b8a22c721d067f39ab60449f
sergey.binder <fornewreal@gmail.com>
2020-09-21 23:53:17 +0700

    "VFX" for bullets

---
+++ Assets/Scripts/Behaviour/Utils/HdrColorAnim.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+using STP.Utils;
+using STP.Utils.GameComponentAttributes;
+
+namespace STP.Behaviour.Utils {
+    public sealed class HdrColorAnim : GameComponent {
+        [NotNull] public HdrSpriteRendererHelper HdrSpriteRendererHelper;
+        [ColorUsage(true, true)]
+        public Color BaseColor = Color.white;
+
+        [Space]
+        public float StartIntensity = 1f;
+        public float MinIntensity   = 1f;
+        public float MaxIntensity   = 2f;
+        public float Speed          = 1f;
+
+        float _intensity = 2f;
+        float _direction = 1f;
+
+        protected override void CheckDescription() {
+            if ( (StartIntensity < MinIntensity) || (StartIntensity > MaxIntensity) ) {
+                StartIntensity = Mathf.Clamp(StartIntensity, MinIntensity, MaxIntensity);
+#if UNITY_EDITOR
+                UnityEditor.EditorUtility.SetDirty(this);
+#endif
+            }
+            if ( MinIntensity > MaxIntensity ) {
+                Debug.LogError("HdrColorAnim: wrong description", this);
+            }
+        }
+
+        void Start() {
+            _intensity = StartIntensity;
+        }
+
+        void Reset() {
+            HdrSpriteRendererHelper = GetComponentInChildren<HdrSpriteRendererHelper>();
+        }
+
+        void Update() {
+            _intensity += _direction * Speed * Time.deltaTime;
+
+            var factor = Mathf.Pow(2,_intensity);
+            var color  = BaseColor;
+            color = new Color(color.r * factor,color.g * factor,color.b * factor, color.a);
+            HdrSpriteRendererHelper.SetColor(color);
+
+            if ( _intensity <= MinIntensity ) {
+                _direction = 1f;
+            } else if ( _intensity >= MaxIntensity ) {
+                _direction = -1f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Behaviour/Utils/HdrSpriteRendererHelper.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+using STP.Utils;
+
+namespace STP.Behaviour.Utils {
+    [RequireComponent(typeof(SpriteRenderer))]
+    [ExecuteInEditMode]
+    public sealed class HdrSpriteRendererHelper : GameComponent {
+        static readonly int HdrColorId = Shader.PropertyToID("_HdrColor");
+
+        [ColorUsage(true, true)]
+        public Color HdrColor = Color.white;
+
+        SpriteRenderer _spriteRenderer;
+        Color          _oldColor;
+
+        MaterialPropertyBlock _mpb;
+
+        SpriteRenderer SpriteRenderer => Application.isPlaying ? _spriteRenderer : GetComponent<SpriteRenderer>();
+
+        MaterialPropertyBlock MaterialPropertyBlock {
+            get {
+                if ( _mpb == null ) {
+                    _mpb = new MaterialPropertyBlock();
+                    SpriteRenderer.GetPropertyBlock(_mpb);
+                }
+                return _mpb;
+            }
+        }
+
+        void OnEnable() {
+            if ( !_spriteRenderer ) {
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            _oldColor = HdrColor;
+            SetColor(HdrColor);
+        }
+
+        void Update() {
+            if ( _oldColor != HdrColor ) {
+                SetColor(HdrColor);
+            }
+        }
+
+        public void SetColor(Color color) {
+            _oldColor = color;
+            MaterialPropertyBlock.SetColor(HdrColorId, _oldColor);
+            SpriteRenderer.SetPropertyBlock(_mpb);
+        }
+    }
+}
