a37e47955576f169a4e53a73e5080236c64fda3b
alexandr.shelekhin <ultrashel@gmail.com>
2020-10-31 20:48:45 +0700

    added ingame fps counter

---
+++ Assets/Scripts/Behaviour/Common/FpsCounter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+using TMPro;
+
+namespace STP.Behaviour.Common {
+    public class FpsCounter : MonoBehaviour {
+        public TMP_Text  _fpsText;
+        public float     _hudRefreshRate = 1f;
+
+        float _timer;
+        void Update() {
+            if ( !(Time.unscaledTime > _timer) ) {
+                return;
+            }
+            var fps = (int)(1f / Time.unscaledDeltaTime);
+            _fpsText.text = $"{fps.ToString()} FPS";
+            _timer = Time.unscaledTime + _hudRefreshRate;
+        }
+    }
+}
