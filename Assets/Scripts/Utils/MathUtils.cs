050f9b42afa789a5511dc8fd46053a625ac3512a
alexandr.shelekhin <ultrashel@gmail.com>
2020-12-24 17:25:03 +0700

    added Lerp for floats

---
+++ Assets/Scripts/Utils/MathUtils.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace STP.Utils {
+    public static class MathUtils {
+        public static float GetSmoothRotationAngleOffset(Vector2 up, Vector2 viewVector, float rotationSpeed) {
+            var angleDiff = -Vector2.SignedAngle(viewVector, up);
+            return LerpFloat(0, angleDiff, rotationSpeed);
+        }
+        
+        public static float LerpFloat(float a, float b, float coeff) {
+            coeff = Mathf.Clamp01(coeff);
+            return (b - a) * coeff + a;
+        }
+    }
+}
