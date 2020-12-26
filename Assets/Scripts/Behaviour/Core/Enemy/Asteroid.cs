605adcdef996ddcb587a9e6623c1bba6149c028c
alexandr.shelekhin <ultrashel@gmail.com>
2020-12-26 19:54:59 +0700

    added rotation and lifetime for asteroids

--- Assets/Scripts/Behaviour/Core/Enemy/Asteroid.cs
+++ Assets/Scripts/Behaviour/Core/Enemy/Asteroid.cs
@@ -1,12 +1,32 @@
 ﻿using UnityEngine;
 
 using STP.Utils;
+using STP.Utils.GameComponentAttributes;
 
 namespace STP.Behaviour.Core.Enemy {
     public class Asteroid : GameComponent {
+        public float LifeTime = 10f;
+        public float RotationPower = 100000f;
+        
+        [NotNull] public Rigidbody2D Rigidbody;
+
+        readonly Timer _lifeTimer = new Timer();
+
+        public void Init(Vector2 force) {
+            Rigidbody.AddRelativeForce(force, ForceMode2D.Impulse);
+            Rigidbody.AddTorque(RotationPower, ForceMode2D.Impulse);
+            _lifeTimer.Start(LifeTime);
+        }
+        
         void OnCollisionEnter2D(Collision2D other) {
             var destructible = other.gameObject.GetComponent<IDestructible>();
             destructible?.TakeDamage(float.MaxValue);
+        }
+
+        void Update() {
+            if ( _lifeTimer.DeltaTick() ) {
+                Destroy(gameObject);
+            }
         }
     }
 }
