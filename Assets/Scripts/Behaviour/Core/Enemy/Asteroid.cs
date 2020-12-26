91009097d144e3545de2ffff0a2109dafc0a3bcd
alexandr.shelekhin <ultrashel@gmail.com>
2020-12-26 18:34:33 +0700

    added static asteroid

---
+++ Assets/Scripts/Behaviour/Core/Enemy/Asteroid.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+using STP.Utils;
+
+namespace STP.Behaviour.Core.Enemy {
+    public class Asteroid : GameComponent {
+        void OnCollisionEnter2D(Collision2D other) {
+            var destructible = other.gameObject.GetComponent<IDestructible>();
+            destructible?.TakeDamage(float.MaxValue);
+        }
+    }
+}
