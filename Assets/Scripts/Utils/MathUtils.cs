a11b6fbf86d4ca5c7e7337eb4654e1e888f81f8b
alexandr.shelekhin <ultrashel@gmail.com>
2020-12-26 20:20:18 +0700

    Revert "added SignedAngle alternative with range [0, 360] instead of [-180, 180]"
    This reverts commit bae05d2ff04e7cfcae6cbca49af7291dded76ff2.

--- Assets/Scripts/Utils/MathUtils.cs
+++ Assets/Scripts/Utils/MathUtils.cs
@@ -3,21 +3,13 @@
 namespace STP.Utils {
     public static class MathUtils {
         public static float GetSmoothRotationAngleOffset(Vector2 up, Vector2 viewVector, float rotationSpeed) {
-            var angleDiff = Angle360(up, viewVector);
+            var angleDiff = -Vector2.SignedAngle(viewVector, up);
             return LerpFloat(0, angleDiff, rotationSpeed);
         }
         
         public static float LerpFloat(float a, float b, float coeff) {
             coeff = Mathf.Clamp01(coeff);
             return (b - a) * coeff + a;
-        }
-
-        public static float Angle360(Vector2 one, Vector2 other) {
-            var res = Vector2.SignedAngle(one, other);
-            if ( res < 0f ) {
-                res += 360f;
-            }
-            return res;
         }
     }
 }
