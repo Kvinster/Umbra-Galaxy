866e4188d5be39ad34b68ffa9ff8d11a0dc98477
alexandr.shelekhin <ultrashel@gmail.com>
2021-01-04 18:59:58 +0700

    added simple level generator

---
+++ Assets/Scripts/Behaviour/Core/LevelGenerator.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+
+using STP.Controller;
+using STP.Utils;
+
+using Random = UnityEngine.Random;
+
+namespace STP.Behaviour.Core {
+	public class LevelGenerator : GameComponent {
+		[Serializable]
+		public class Chunk {
+			public int        Weight;
+			public GameObject Prefab;
+		}
+
+		public int CellSideSize  = 300;
+		public int SpaceSideSize = 10000;
+
+		public List<Chunk> LevelChunks;
+
+
+		public void GenerateLevel(LevelController levelController) {
+			var levelConfig = levelController.GetCurLevelConfig();
+			var minPoint    = new Vector2(-SpaceSideSize / 2.0f, -SpaceSideSize / 2.0f);
+			var maxPoint    = new Vector2( SpaceSideSize / 2.0f,  SpaceSideSize / 2.0f);
+			Random.InitState(levelConfig.LevelSeed);
+			for ( var y = minPoint.y; y < maxPoint.y; y += CellSideSize ) {
+				for ( var x = minPoint.x; x < maxPoint.x; x += CellSideSize ) {
+					var chunk = GetRandomChunk();
+					Instantiate(chunk, new Vector3(x, y, 0), Quaternion.identity);
+				}
+			}
+		}
+
+		GameObject GetRandomChunk() {
+			var totalWeight = 0;
+			foreach ( var chunk in LevelChunks ) {
+				totalWeight += chunk.Weight;
+			}
+
+			var randomValue = Random.Range(0, totalWeight);
+			foreach ( var chunk in LevelChunks ) {
+				if ( chunk.Weight > randomValue ) {
+					return chunk.Prefab;
+				}
+
+				randomValue -= chunk.Weight;
+			}
+
+			return null;
+		}
+	}
+}
--- Assets/Scripts/Behaviour/Starter/CoreStarter.cs
+++ Assets/Scripts/Behaviour/Starter/CoreStarter.cs
@@ -1,30 +1,32 @@
 ﻿using UnityEngine;
 
 using STP.Behaviour.Core;
+using STP.Controller;
 using STP.Manager;
 using STP.Utils.GameComponentAttributes;
 using STP.View.DebugGUI;
 
 namespace STP.Behaviour.Starter {
-    public class CoreStarter : BaseStarter<CoreStarter> {
-        [NotNull] public Player    Player;
-        [NotNull] public Transform PlayerStartPos;
+	public class CoreStarter : BaseStarter<CoreStarter> {
+		[NotNull] public Player         Player;
+		[NotNull] public Transform      PlayerStartPos;
+		[NotNull] public LevelGenerator Generator;
 
-        public LevelGoalManager LevelGoalManager { get; private set; }
+		public LevelGoalManager LevelGoalManager { get; private set; }
 
-        void Start() {
-            LevelGoalManager = new LevelGoalManager(Player.transform);
+		void Start() {
+			LevelGoalManager = new LevelGoalManager(Player.transform);
+			Generator.GenerateLevel(LevelController.Instance);
+			InitComponents();
+			// Settings for smooth gameplay
+			Application.targetFrameRate = Screen.currentResolution.refreshRate;
+			QualitySettings.vSyncCount  = 0;
+		}
 
-            InitComponents();
-            // Settings for smooth gameplay
-            Application.targetFrameRate = Screen.currentResolution.refreshRate;
-            QualitySettings.vSyncCount  = 0;
-        }
-
-        void OnDestroy() {
-            if ( DebugGuiController.HasInstance ) {
-                DebugGuiController.Instance.SetDrawable(null);
-            }
-        }
-    }
+		void OnDestroy() {
+			if ( DebugGuiController.HasInstance ) {
+				DebugGuiController.Instance.SetDrawable(null);
+			}
+		}
+	}
 }
