ffaddac67b8394579f5c8ad7fbdd9b8dde9051b3
alexandr.shelekhin <ultrashel@gmail.com>
2021-01-04 18:58:34 +0700

    added current level params controller

---
+++ Assets/Scripts/Config/LevelsConfig.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace STP.Config {
+	[Serializable]
+	public class LevelInfo {
+		public int    GeneratorsCount = 1;
+		public int    LevelSeed       = 0;
+		public string LevelName       = "Level1";
+		public string NextLevelName   = "Level2";
+	}
+	
+	[CreateAssetMenu(fileName = "AllLevels", menuName = "ScriptableObjects/LevelsConfig", order = 1)]
+	public class LevelsConfig : ScriptableObject {
+		public List<LevelInfo> Levels;
+
+		public LevelInfo GetLevelConfig(string levelName) {
+			return Levels.Find(x => (x.LevelName == levelName));
+		}
+	}
+}
---
+++ Assets/Scripts/Controller/LevelController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+using STP.Config;
+using STP.Utils;
+
+namespace STP.Controller {
+	public class LevelController : Singleton<LevelController> {
+		const string DefaultLevelName = "Level1";
+
+		LevelsConfig _levelsConfig;
+
+		public string CurLevelName { get; private set; }
+		
+		public LevelController() {
+			CurLevelName = DefaultLevelName;
+			LoadConfig();
+		}
+
+		public void ChangeLevel(string newLevelName) {
+			CurLevelName = newLevelName;
+		}
+
+		public LevelInfo GetCurLevelConfig() {
+			return _levelsConfig.GetLevelConfig(CurLevelName);
+		}
+
+		void LoadConfig() {
+			_levelsConfig = Resources.Load<LevelsConfig>("AllLevels");
+		}
+	}
+}
