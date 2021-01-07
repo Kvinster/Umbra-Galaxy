c2a49519a31deb41078f2c7126aad2ebb548d2ff
sergey.binder <fornewreal@gmail.com>
2021-01-07 17:27:29 +0700

    VFX for collision between bullets and player shield

---
+++ Assets/Scripts/Behaviour/Core/PlayerShield.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+using STP.Behaviour.Starter;
+using STP.Utils.GameComponentAttributes;
+
+namespace STP.Behaviour.Core {
+	[RequireComponent(typeof(Collider2D))]
+	public sealed class PlayerShield : BaseCoreComponent {
+		[NotNull] public VisualEffect VisualEffect;
+
+		protected override void InitInternal(CoreStarter starter) {
+		}
+
+		void OnCollisionEnter2D(Collision2D other) {
+			if ( other.gameObject.GetComponent<Bullet>() ) {
+				var worldContact = other.contacts[0].point;
+				VisualEffect.SetVector2("CollisionPos", VisualEffect.transform.InverseTransformPoint(worldContact));
+				VisualEffect.SetVector2("CollisionDirection", (worldContact - (Vector2) transform.position).normalized);
+				VisualEffect.SendEvent("OnCollision");
+			}
+		}
+	}
+}
