2d031fa1eb4e5be19e15e23b010ad78f681e2a70
alexandr.shelekhin <ultrashel@gmail.com>
2021-01-17 16:47:50 +0700

    added Random utils

---
+++ Assets/Scripts/Utils/RandomUtils.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+using System.Linq;
+
+using STP.Config;
+
+namespace STP.Utils {
+	public static class RandomUtils {
+		public static T GetRandomElement<T>(List<T> collection) {
+			if ( collection.Count == 0 ) {
+				Debug.LogWarning("Collection is empty. Returning default value");
+				return default;
+			}
+			var index = Random.Range(0, collection.Count);
+			return collection[index];
+		}
+
+
+		public static T GetAndRemoveRandomElement<T>(List<T> collection) {
+			if ( collection.Count == 0 ) {
+				Debug.LogWarning("Collection is empty. Returning default value");
+				return default;
+			}
+			var index = Random.Range(0, collection.Count);
+			var res   = collection[index];
+			collection.RemoveAt(index);
+			return res;
+		}
+
+		public static T GetRandomWeightedElement<T>(List<T> collection) where T : WeightedValue {
+			var totalWeight = collection.Sum(chunk => chunk.Weight);
+			var randomValue = Random.Range(0, totalWeight);
+			foreach ( var obj in collection ) {
+				if ( obj.Weight > randomValue ) {
+					return obj;
+				}
+				randomValue -= obj.Weight;
+			}
+			Debug.LogWarning("Something went wrong. Returning default value");
+			return default;
+		}
+	}
+}
