be286f9ae5a29e1efb232699d7dc944eb29ed055
sergey.binder <fornewreal@gmail.com>
2021-01-31 12:25:23 +0700

    Separate component for enemy death sound player

---
+++ Assets/Scripts/Behaviour/Core/Enemy/SimpleEnemyDeathSoundPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+using STP.Behaviour.Utils;
+using STP.Utils;
+using STP.Utils.GameComponentAttributes;
+
+namespace STP.Behaviour.Core.Enemy {
+	public sealed class SimpleEnemyDeathSoundPlayer : GameComponent {
+		[NotNull] public DelayedDestroyer      DeathSoundDestroyer;
+		[NotNull] public Transform             DeathSoundPlayerTransform;
+		[NotNull] public BaseSimpleSoundPlayer DeathSoundPlayer;
+
+		Transform _tmpObjRoot;
+
+		public void Init(Transform tmpObjRoot) {
+			_tmpObjRoot = tmpObjRoot;
+		}
+
+		public void Play() {
+			DeathSoundPlayerTransform.SetParent(_tmpObjRoot);
+			DeathSoundPlayerTransform.position = transform.position;
+			DeathSoundDestroyer.StartDestroy();
+			DeathSoundPlayer.Play();
+		}
+	}
+}
