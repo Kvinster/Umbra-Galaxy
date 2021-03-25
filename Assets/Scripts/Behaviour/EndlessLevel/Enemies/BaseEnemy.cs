614abab76d47a3ce7e8dfc0b18181ed9e2a39541
alexandr.shelekhin <ultrashel@gmail.com>
2021-03-25 23:02:31 +0700

    added base waves

---
+++ Assets/Scripts/Behaviour/EndlessLevel/Enemies/BaseEnemy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+using System;
+
+using STP.Behaviour.Core;
+using STP.Behaviour.Starter;
+using STP.Events;
+using STP.Utils.Events;
+using STP.Utils.GameComponentAttributes;
+
+namespace STP.Behaviour.EndlessLevel.Enemies {
+	public abstract class BaseEnemy : BaseEndlessLevelComponent {
+		[NotNullOrEmpty] public string Name;
+		[Space]
+		public SimpleDeathSoundPlayer DeathSoundPlayer;
+
+		public event Action<BaseEnemy> OnDestroyed;
+		
+		protected bool IsAlive;
+		
+		protected override void InitInternal(EndlessLevelStarter starter) {
+			if ( DeathSoundPlayer ) {
+				DeathSoundPlayer.Init(starter.TmpObjectsRoot);
+			}
+			IsAlive = true;
+		}
+
+		protected virtual void Die(bool fromPlayer = true) {
+			if ( !IsAlive ) {
+				return;
+			}
+			OnDestroyed?.Invoke(this);
+			IsAlive = false;
+			TryStartPlayingDeathSound();
+			if ( fromPlayer ) {
+				EventManager.Fire(new EnemyDestroyed(Name));
+			}
+		}
+
+		protected void TryStartPlayingDeathSound() {
+			if ( DeathSoundPlayer ) {
+				DeathSoundPlayer.Play();
+			}
+		}
+	}
+}
--- Assets/Scripts/Behaviour/Starter/EndlessLevelStarter.cs
+++ Assets/Scripts/Behaviour/Starter/EndlessLevelStarter.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 
+using STP.Behaviour.EndlessLevel;
 using STP.Utils.GameComponentAttributes;
 
 namespace STP.Behaviour.Starter {
 	public sealed class EndlessLevelStarter : BaseStarter<EndlessLevelStarter> {
-		[NotNull] public Camera Camera;
-
+		[NotNull] public Camera             Camera;
+		[NotNull] public EndlessLevelPlayer Player;
+		[NotNull] public Transform          TmpObjectsRoot;
+ 
 		void Start() {
 			InitComponents();
 		}
