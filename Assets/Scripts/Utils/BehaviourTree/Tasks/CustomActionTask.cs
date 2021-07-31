51750cb959a71f11ab2c8f793ac9c7b039ed3169
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-07-31 16:40:53 +0700

    added custom action tasks

---
+++ Assets/Scripts/Utils/BehaviourTree/Tasks/CustomActionTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace STP.Utils.BehaviourTree.Tasks {
+	public class CustomActionTask : BaseTask {
+		readonly Func<TaskStatus> _task;
+		
+		public CustomActionTask(Func<TaskStatus> task) {
+			_task = task;
+		}
+
+		protected override TaskStatus ExecuteInternal() {
+			return _task();
+		}
+	}
+}
