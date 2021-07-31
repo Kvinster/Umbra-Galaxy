671d1a06258dde0ee8fa72127bc0008d1d384ee7
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-07-31 16:40:43 +0700

    Added condition tasks

---
+++ Assets/Scripts/Utils/BehaviourTree/Tasks/ConditionTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace STP.Utils.BehaviourTree.Tasks {
+	public class ConditionTask : BaseTask {
+		readonly Func<bool> _conditionChecker;
+		
+		public ConditionTask(Func<bool> conditionChecker) {
+			_conditionChecker = conditionChecker;
+		}
+
+		protected override TaskStatus ExecuteInternal() {
+			return _conditionChecker() ? TaskStatus.Success : TaskStatus.Failure;
+		}
+	}
+}
