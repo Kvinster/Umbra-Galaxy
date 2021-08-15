d8c3b4f041e1dd267601c098ab1d5ed1f90ab863
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-08-15 10:29:57 +0700

    added AlwaysSuccess task decorator

---
+++ Assets/Scripts/Utils/BehaviourTree/Tasks/AlwaysSuccessDecorator.cs
@@ -0,0 +1,15 @@
+﻿namespace STP.Utils.BehaviourTree.Tasks {
+	public class AlwaysSuccessDecorator : BaseTask {
+		public AlwaysSuccessDecorator(BaseTask task) {
+			SubTasks.Add(task);
+		}
+		
+		protected override TaskStatus ExecuteInternal() {
+			var res = SubTasks[0].Execute();
+			if (res == TaskStatus.Continue) {
+				return TaskStatus.Continue;
+			}
+			return TaskStatus.Success;
+		}
+	}
+}
