ba31eff8f90af3aca3388210258e83a671af4066
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-08-31 22:38:16 +0700

    added tasks for infinite loops and parallel execution in behaviour treee

---
+++ Assets/Scripts/Utils/BehaviourTree/Tasks/RepeatForeverTask.cs
@@ -0,0 +1,16 @@
+﻿namespace STP.Utils.BehaviourTree.Tasks {
+	public sealed class RepeatForeverTask : BaseTask {
+		
+		public RepeatForeverTask(BaseTask task) : base(nameof(RepeatForeverTask)) {
+			SubTasks.Add(task);
+		}
+
+		protected override TaskStatus ExecuteInternal() {
+			var res = SubTasks[0].Execute();
+			if ( res != TaskStatus.Continue ) {
+				SubTasks[0].ResetStatus();
+			}
+			return TaskStatus.Continue;
+		}
+	}
+}
