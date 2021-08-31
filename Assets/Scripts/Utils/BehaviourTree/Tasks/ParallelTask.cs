a8745e7889ca1d574df983421ef0745f840465f0
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-08-31 22:38:16 +0700

    added tasks for infinite loops and parallel execution in behaviour treee

---
+++ Assets/Scripts/Utils/BehaviourTree/Tasks/ParallelTask.cs
@@ -0,0 +1,21 @@
+﻿namespace STP.Utils.BehaviourTree.Tasks {
+	public sealed class ParallelTask : BaseTask {
+		public ParallelTask(params BaseTask[] task) : base(nameof(ParallelTask)) {
+			SubTasks.AddRange(task);
+		}
+		
+		protected override TaskStatus ExecuteInternal() {
+			var hasRunningTasks = false;
+			foreach ( var task in SubTasks ) {
+				var res = task.Execute();
+				if ( res == TaskStatus.Failure ) {
+					return TaskStatus.Failure;
+				}
+				if ( res == TaskStatus.Continue ) {
+					hasRunningTasks = true;
+				}
+			}
+			return (hasRunningTasks) ? TaskStatus.Continue : TaskStatus.Success;
+		}
+	}
+}
