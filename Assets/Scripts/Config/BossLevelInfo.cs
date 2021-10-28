bb84cc1a844e2de43e36ad411c77107c80916b4b
sergey.binder <fornewreal@gmail.com>
2021-10-28 19:11:56 +0300

    Removed unused field

--- Assets/Scripts/Config/BossLevelInfo.cs
+++ Assets/Scripts/Config/BossLevelInfo.cs
@@ -3,15 +3,6 @@
 namespace STP.Config {
 	[CreateAssetMenu(menuName = "ScriptableObjects/" + nameof(BossLevelInfo), fileName = nameof(BossLevelInfo))]
 	public sealed class BossLevelInfo : BaseLevelInfo {
-		public int BossChunkIndex;
-
 		public override LevelType LevelType => LevelType.Boss;
-
-		void OnValidate() {
-			if ( BossChunkIndex < 0 ) {
-				Debug.LogErrorFormat(this, "{0}.{1}: {2} can't be below zero", nameof(BossLevelInfo),
-					nameof(OnValidate), nameof(BossChunkIndex));
-			}
-		}
 	}
 }
