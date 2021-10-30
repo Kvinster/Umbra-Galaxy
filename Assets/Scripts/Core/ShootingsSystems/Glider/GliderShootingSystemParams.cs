6e5ffd49a9f0fe95c5294a1072658bd3bb3d5204
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-10-30 20:49:56 +0700

    removed gliders

--- Assets/Scripts/Core/ShootingsSystems/Glider/GliderShootingSystemParams.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace STP.Core.ShootingsSystems.Glider {
-	[Serializable]
-	public sealed class GliderShootingSystemParams : ShootingSystemParams {
-		public float MinAttackDistance;
-		public float MaxAttackDistance;
-	}
-}
