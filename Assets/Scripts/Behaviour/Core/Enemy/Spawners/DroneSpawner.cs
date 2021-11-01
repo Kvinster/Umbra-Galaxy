87a9af296c681ae3a2cbbfeedaaf60b0db873c56
sergey.binder <fornewreal@gmail.com>
2021-11-01 21:35:49 +0300

    Drones from DroneSpawner are targeted on player

--- Assets/Scripts/Behaviour/Core/Enemy/Spawners/DroneSpawner.cs
+++ Assets/Scripts/Behaviour/Core/Enemy/Spawners/DroneSpawner.cs
@@ -1,15 +1,27 @@
-﻿using STP.Behaviour.Starter;
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+using STP.Behaviour.Starter;
 using STP.Config;
 
 namespace STP.Behaviour.Core.Enemy.Spawners {
 	public sealed class DroneSpawner : BaseSpawner {
 		BaseSpawnerSettings _settings;
-		
+
+		Transform _playerTransform;
+
 		protected override BaseSpawnerSettings Settings => _settings;
-		
+
 		protected override void InitInternal(CoreStarter starter) {
-			_settings = starter.LevelController.CurLevelConfig.DroneSpawnerSettings;
+			_settings        = starter.LevelController.CurLevelConfig.DroneSpawnerSettings;
+			_playerTransform = starter.Player.transform;
 			base.InitInternal(starter);
+		}
+
+		protected override void InitItem(GameObject go) {
+			var drone = go.GetComponent<Drone>();
+			Assert.IsTrue(drone);
+			drone.SetTarget(_playerTransform);
 		}
 	}
 }
