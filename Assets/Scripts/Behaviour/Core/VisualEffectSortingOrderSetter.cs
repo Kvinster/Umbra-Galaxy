afb39a44c5cf0888a336656fc3c027c909c3406f
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-11-11 23:36:33 +0700

    added script for setting sorting order/layer to visual effects

---
+++ Assets/Scripts/Behaviour/Core/VisualEffectSortingOrderSetter.cs
@@ -0,0 +1,23 @@
+﻿using STP.Utils;
+using STP.Utils.GameComponentAttributes;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace STP.Behaviour.Core {
+	[RequireComponent(typeof(VisualEffect))]
+	public class VisualEffectSortingOrderSetter : GameComponent {
+		[NotNull] public Renderer Renderer;
+		
+		public int    SortingOrder;
+		public string SortingLayer;
+
+		public void Reset() {
+			Renderer = gameObject.GetComponent<Renderer>();
+		}
+
+		public void Start() {
+			Renderer.sortingOrder     = SortingOrder;
+			Renderer.sortingLayerName = SortingLayer;
+		}
+	}
+}
