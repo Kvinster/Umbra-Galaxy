981f357b7d72e583cd2290ec4498c8726c381f7d
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-11-13 15:39:41 +0700

    added chunk and level for the second boss

---
+++ Assets/Scripts/Behaviour/Core/Enemy/SecondBoss/MineSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using STP.Utils;
+using STP.Utils.GameComponentAttributes;
+using UnityEngine;
+
+namespace STP.Behaviour.Core.Enemy.BossSpawner {
+	public class MineSpawner : GameComponent {
+		[NotNullOrEmpty] public GameObject MinePrefab;
+
+		
+		CoreSpawnHelper _spawnHelper;
+		Transform       _bossTransform;
+		
+		
+		public void Init(CoreSpawnHelper spawnHelper, Transform bossTransform) {
+			_spawnHelper = spawnHelper;
+			_bossTransform        = bossTransform;
+		}
+
+		public void Spawn() {
+			var enemy = Instantiate(MinePrefab, transform.position, Quaternion.identity);
+			_spawnHelper.TryInitSpawnedObject(enemy);
+			var enemyRigidbody = enemy.GetComponent<Rigidbody2D>();
+			if ( !enemyRigidbody ) {
+				Debug.LogError("Can't get rigidbody from spawned mines");
+				return;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Behaviour/Core/Enemy/SecondBoss/SecondBossController.cs
@@ -0,0 +1,12 @@
+﻿using STP.Behaviour.Core.Enemy.BossSpawner;
+using STP.Behaviour.Starter;
+using STP.Utils.GameComponentAttributes;
+
+namespace STP.Behaviour.Core.Enemy.SecondBoss {
+	public class SecondBossController : BaseCoreComponent {
+		[NotNull] public BossMovementSubsystem MovementSubsystem;
+		
+		protected override void InitInternal(CoreStarter starter) {
+		}
+	}
+}
