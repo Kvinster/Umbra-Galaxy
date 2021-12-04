9d98a9fae689cbf9f01df0675206dc9f863c1768
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-12-04 14:06:32 +0700

    simplified rotation effect

--- Assets/Scripts/Behaviour/MainMenu/RotationEffect.cs
+++ Assets/Scripts/Behaviour/MainMenu/RotationEffect.cs
@@ -1,12 +1,9 @@
-﻿using DG.Tweening;
-using STP.Utils;
+﻿using STP.Utils;
 using UnityEngine;
 
 namespace STP.Behaviour.MainMenu {
 	public sealed class RotationEffect : GameComponent {
 		public float CircleTime;
-
-		Sequence _seq;
 
 		public void ShowEffect() {
 			gameObject.SetActive(true);
@@ -16,17 +13,10 @@ namespace STP.Behaviour.MainMenu {
 			gameObject.SetActive(false);
 		}
 
-		void OnEnable() {
-			_seq?.Kill();
-			_seq = DOTween.Sequence();
-			var endRotation = transform.rotation.eulerAngles + Vector3.forward * 360;
-			_seq.Append(transform.DORotate(endRotation, CircleTime, RotateMode.FastBeyond360))
-				.SetAutoKill(false)
-				.SetLoops(-1);
-		}
-
-		void OnDisable() {
-			_seq?.Kill();
+		void Update() {
+			if ( gameObject.activeSelf ) {
+				transform.Rotate(Vector3.forward * (360f / CircleTime) * Time.unscaledDeltaTime);
+			}
 		}
 	}
 }
