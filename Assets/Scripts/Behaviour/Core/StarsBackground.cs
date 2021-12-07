949a59d0010f835820b76e09d85f044859405b09
sergey.binder <fornewreal@gmail.com>
2021-12-07 21:10:59 +0700

    Stars background vfx

---
+++ Assets/Scripts/Behaviour/Core/StarsBackground.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.VFX;
+
+using STP.Behaviour.Starter;
+using STP.Utils.GameComponentAttributes;
+
+namespace STP.Behaviour.Core {
+	public sealed class StarsBackground : BaseCoreComponent {
+		const string AreaPosId  = "AreaPos";
+		const string AreaSizeId = "AreaSize";
+
+		[NotNull] public VisualEffect VisualEffect;
+
+		protected override void InitInternal(CoreStarter starter) {
+			VisualEffect.SetVector2(AreaPosId, starter.AreaRect.center);
+			VisualEffect.SetVector2(AreaSizeId, starter.AreaRect.size);
+		}
+	}
+}
