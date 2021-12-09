8d5fc584cd16c5dfc36c14dad1df8f12e4a1c56e
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-12-09 23:00:44 +0700

    added appearing effect for the leaderboard in the win window

--- Assets/Scripts/Behaviour/Core/UI/WinWindow/WinWindow.cs
+++ Assets/Scripts/Behaviour/Core/UI/WinWindow/WinWindow.cs
@@ -13,12 +13,18 @@ using IPromise = RSG.IPromise;
 
 namespace STP.Behaviour.Core.UI.WinWindow {
 	public sealed class WinWindow : BaseCoreWindow {
+		[NotNull] public Transform   Title;
+		[NotNull] public CanvasGroup TitleCanvasGroup;
+		
 		[NotNull] public List<LeaderboardEntryView> Entries;
 		[NotNull] public GameObject                 EntriesRoot;
 
 		[NotNull] public Button ContinueButton;
 
-		[NotNull] public GameObject LoadingRoot;
+		[NotNull] public GameObject  LoadingRoot;
+		[NotNull] public CanvasGroup LoadingCanvasGroup;
+		
+		[NotNull] public CanvasGroup LeaderboardRootCanvasGroup;
 
 		LeaderboardController _leaderboardController;
 		LevelManager          _levelManager;
@@ -28,6 +34,8 @@ namespace STP.Behaviour.Core.UI.WinWindow {
 
 		LeaderboardEntryView _activePlayerView;
 
+		Sequence _activeSequence;
+		
 		public void CommonInit(ScoreController scoreController, LeaderboardController leaderboardController, LevelManager levelManager) {
 			_scoreController       = scoreController;
 			_leaderboardController = leaderboardController;
@@ -37,12 +45,45 @@ namespace STP.Behaviour.Core.UI.WinWindow {
 
 		public override IPromise Show() {
 			var promise = base.Show();
+
+			_activeSequence = CreateLoadingAnimation().AppendCallback(() => _activeSequence = null);
+			
 			LoadingRoot.gameObject.SetActive(true);
 			EntriesRoot.SetActive(false);
 			UpdateLeaderboard().Forget();
 			return promise;
 		}
 
+		Sequence CreateLoadingAnimation() {
+			var endPos = Title.position;
+
+			Title.position           = Vector3.zero;
+			TitleCanvasGroup.alpha   = 0f;
+			LoadingCanvasGroup.alpha = 0f;
+
+			var seq = DOTween.Sequence()
+				.Append(TitleCanvasGroup.DOFade(1f, 0.3f))
+				.Append(Title.DOMove(endPos, 0.3f))
+				.Append(LoadingCanvasGroup.DOFade(1f, 0.3f))
+				.SetUpdate(true);
+			return seq;
+		}
+
+		Sequence CreateShowLeaderboardAnimation() {
+			LeaderboardRootCanvasGroup.alpha = 0f;
+			
+			var seq = DOTween.Sequence()
+				.Append(LoadingCanvasGroup.DOFade(0f, 0.3f))
+				.AppendCallback(() => {
+					LoadingCanvasGroup.gameObject.SetActive(false);
+					LeaderboardRootCanvasGroup.gameObject.SetActive(true);
+					InitViews();
+				})
+				.Append(LeaderboardRootCanvasGroup.DOFade(1f, 0.3f))
+				.SetUpdate(true);
+			return seq;
+		}
+		
 		void InitViews() {
 			if ( _scores == null ) {
 				return;
@@ -89,10 +130,12 @@ namespace STP.Behaviour.Core.UI.WinWindow {
 			await _leaderboardController.TryLoginAsync();
 			await _leaderboardController.PublishScoreAsync(_scoreController.Score);
 			await UniTask.Delay(1000, DelayType.UnscaledDeltaTime);
-			_scores = await _leaderboardController.GetScoresAroundPlayerAsync(Entries.Count);
-			LoadingRoot.gameObject.SetActive(false);
-			EntriesRoot.SetActive(true);
-			InitViews();
+			_scores             = await _leaderboardController.GetScoresAroundPlayerAsync(Entries.Count);
+			if ( _activeSequence == null ) {
+				_activeSequence = CreateShowLeaderboardAnimation();
+			} else {
+				_activeSequence.Append(CreateShowLeaderboardAnimation());
+			}
 		}
 
 		void OnContinueClick() {
