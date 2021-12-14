0b51e4c0dc21cebccdd04a5d91ada9e27c5e4741
sergey.binder <fornewreal@gmail.com>
2021-12-14 21:38:36 +0700

    Music in core and main menu

--- Assets/Scripts/Behaviour/Core/CoreMusicPlayer.cs
+++ Assets/Scripts/Behaviour/Core/CoreMusicPlayer.cs
@@ -1,12 +1,21 @@
-﻿using STP.Behaviour.Sound;
+﻿using UnityEngine;
+
+using STP.Behaviour.Sound;
 using STP.Behaviour.Starter;
+using STP.Config;
 using STP.Utils.GameComponentAttributes;
 
 namespace STP.Behaviour.Core {
 	public sealed class CoreMusicPlayer : BaseCoreComponent {
+		[Header("Parameters")]
+		public bool DisableOnBossLevel;
+		[Header("Dependencies")]
 		[NotNull] public MusicPlayer MusicPlayer;
 
 		protected override void InitInternal(CoreStarter starter) {
+			if ( DisableOnBossLevel && (starter.LevelController.CurLevelType == LevelType.Boss) ) {
+				return;
+			}
 			MusicPlayer.Play();
 		}
 	}
