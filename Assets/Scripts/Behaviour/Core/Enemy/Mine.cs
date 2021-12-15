ec463395188a4192a3da1e1e2024fa0960a59200
sergey.binder <fornewreal@gmail.com>
2021-12-15 22:07:33 +0700

    Mine (as in bomb) spawn SFX

--- Assets/Scripts/Behaviour/Core/Enemy/Mine.cs
+++ Assets/Scripts/Behaviour/Core/Enemy/Mine.cs
@@ -1,15 +1,21 @@
-﻿using STP.Utils;
-using UnityEngine;
+﻿using UnityEngine;
+
+using STP.Utils;
+using STP.Utils.GameComponentAttributes;
 
 namespace STP.Behaviour.Core.Enemy {
 	public class Mine : BaseEnemy {
+		[Header("Parameters")]
 		public float DamageOnCollision = int.MaxValue;
 		public float Lifetime          = 5f;
+		[Header("Dependencies")]
+		[NotNull] public BaseSimpleSoundPlayer SpawnSoundPlayer;
 
-		Timer _timer = new Timer();
+		readonly Timer _timer = new Timer();
 
 		void Start() {
 			_timer.Reset(Lifetime);
+			SpawnSoundPlayer.Play();
 		}
 
 		void Update() {
